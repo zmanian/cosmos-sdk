de21eb486608456c260009fd1a8fe741b1c72171
Adrian Brink <adrian@brink-holdings.com>
2017-07-23 19:49:46 -0400

    Enable gomobile bindings
    Signed-off-by: Adrian Brink <adrian@brink-holdings.com>

--- modules/coin/bindings/query.go
+++ modules/coin/bindings/query.go
@@ -1,32 +1,31 @@
-package main
+package bindings
 
 import (
-  "encoding/hex"
+	"encoding/hex"
 
-  "github.com/tendermint/basecoin/client/commands"
-  // proofcmd "github.com/tendermint/basecoin/client/commands/proofs"
-  "github.com/tendermint/basecoin/modules/coin"
-  "github.com/tendermint/basecoin/stack"
+	"github.com/tendermint/basecoin/client/commands"
+	// proofcmd "github.com/tendermint/basecoin/client/commands/proofs"
+	"github.com/tendermint/basecoin/modules/coin"
+	"github.com/tendermint/basecoin/stack"
 )
 
-func main() {}
-
 type ExportCoin struct {
-  Denom  string `json:"denom"`
-  Amount int  `json:"amount"`
+	Denom  string `json:"denom"`
+	Amount int    `json:"amount"`
 }
 
 type ExportAccount struct {
-  Coins []ExportCoin
+	Coins []ExportCoin
 }
 
 type AccountResult struct {
-  Height int
-  Key string
-  Account ExportAccount
-  Error string
+	Height  int
+	Key     string
+	Account ExportAccount
+	Error   string
 }
 
+/*
 func convertAccount(acct coin.Account) ExportAccount {
   return ExportAccount{
     Coins: convertCoins(acct.Coins),
@@ -43,34 +42,34 @@ func convertCoins(coins []coin.Coin) []ExportCoin {
   }
   return res
 }
-
+*/
 func GetAccount(hexAddr string) AccountResult {
-  act, err := commands.ParseActor(hexAddr)
-  if err != nil {
-    return AccountResult{Error: err.Error()}
-  }
-  key := stack.PrefixedKey(coin.NameCoin, act.Bytes())
+	act, err := commands.ParseActor(hexAddr)
+	if err != nil {
+		return AccountResult{Error: err.Error()}
+	}
+	key := stack.PrefixedKey(coin.NameCoin, act.Bytes())
 
-  return AccountResult{
-    Height: 50,
-    Key: hex.EncodeToString(key),
-    Account: ExportAccount{
-      Coins: []ExportCoin{{
-        Denom: "atom",
-        Amount: 420,
-      }},
-    },
-  }
+	return AccountResult{
+		Height: 50,
+		Key:    hex.EncodeToString(key),
+		Account: ExportAccount{
+			Coins: []ExportCoin{{
+				Denom:  "atom",
+				Amount: 420,
+			}},
+		},
+	}
 
-  // acct := coin.Account{}
-  // proof, err := proofcmd.GetAndParseAppProof(key, &acct)
-  // if err != nil {
-  //   return AccountResult{Error: err.Error()}
-  // }
+	// acct := coin.Account{}
+	// proof, err := proofcmd.GetAndParseAppProof(key, &acct)
+	// if err != nil {
+	//   return AccountResult{Error: err.Error()}
+	// }
 
-  // res := AccountResult{
-  //   Height: int(proof.BlockHeight()),
-  //   Account: convertAccount(acct),
-  // }
-  // return res
+	// res := AccountResult{
+	//   Height: int(proof.BlockHeight()),
+	//   Account: convertAccount(acct),
+	// }
+	// return res
 }
