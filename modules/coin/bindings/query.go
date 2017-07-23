1c71d5967de1e2bdcd5602675baae393471b58e8
Ethan Frey <ethanfrey@users.noreply.github.com>
2017-07-23 19:37:50 -0400

    Remove network calls, config...

--- modules/coin/bindings/query.go
+++ modules/coin/bindings/query.go
@@ -1,16 +1,47 @@
-package bindings
+package main
 
 import (
+  "encoding/hex"
+
   "github.com/tendermint/basecoin/client/commands"
-  proofcmd "github.com/tendermint/basecoin/client/commands/proofs"
+  // proofcmd "github.com/tendermint/basecoin/client/commands/proofs"
   "github.com/tendermint/basecoin/modules/coin"
   "github.com/tendermint/basecoin/stack"
 )
 
+func main() {}
+
+type ExportCoin struct {
+  Denom  string `json:"denom"`
+  Amount int  `json:"amount"`
+}
+
+type ExportAccount struct {
+  Coins []ExportCoin
+}
+
 type AccountResult struct {
   Height int
-  Account coin.Account
+  Key string
+  Account ExportAccount
   Error string
+}
+
+func convertAccount(acct coin.Account) ExportAccount {
+  return ExportAccount{
+    Coins: convertCoins(acct.Coins),
+  }
+}
+
+func convertCoins(coins []coin.Coin) []ExportCoin {
+  res := make([]ExportCoin, len(coins))
+  for i, c := range coins {
+    res[i] = ExportCoin{
+      Denom: c.Denom,
+      Amount: int(c.Amount),
+    }
+  }
+  return res
 }
 
 func GetAccount(hexAddr string) AccountResult {
@@ -20,15 +51,26 @@ func GetAccount(hexAddr string) AccountResult {
   }
   key := stack.PrefixedKey(coin.NameCoin, act.Bytes())
 
-  acct := coin.Account{}
-  proof, err := proofcmd.GetAndParseAppProof(key, &acct)
-  if err != nil {
-    return AccountResult{Error: err.Error()}
+  return AccountResult{
+    Height: 50,
+    Key: hex.EncodeToString(key),
+    Account: ExportAccount{
+      Coins: []ExportCoin{{
+        Denom: "atom",
+        Amount: 420,
+      }},
+    },
   }
 
-  res := AccountResult{
-    Height: int(proof.BlockHeight()),
-    Account: acct,
-  }
-  return res
+  // acct := coin.Account{}
+  // proof, err := proofcmd.GetAndParseAppProof(key, &acct)
+  // if err != nil {
+  //   return AccountResult{Error: err.Error()}
+  // }
+
+  // res := AccountResult{
+  //   Height: int(proof.BlockHeight()),
+  //   Account: convertAccount(acct),
+  // }
+  // return res
 }
